7216c6465ecfaf1a9ebe0b6332e6b208fd901e20
Marcello Messori <marcello.mm.messori@gmail.com>
2017-03-05 00:36:29 +0100

    REF client/public/app.js by removing jQuery - step2

--- client/public/app.js
+++ client/public/app.js
@@ -1,62 +1,3 @@
-$(document).ready(function(){
-
-  $('#doc-fields-clear').click(function(){
-	$('.doc-input-field').val('');
-	$('#form-files').html('');
-  });
-
-  $('#upload').click(function () {
-	var request = new XMLHttpRequest();
-	request.open('POST', '/api/files');
-	request.onreadystatechange = function() {
-	  if (request.readyState === XMLHttpRequest.DONE) {
-		if (request.status === 201) {
-		  let responseObj = JSON.parse(request.responseText)
-		    , responseObjKeys = Object.keys(responseObj);
-
-		  for (let i=0; i<responseObjKeys.length; i++) {
-			document.getElementById('file_' + i + '_path').value = responseObj[i][0].path;
-		  }
-
-		  uploadDocument();
-		} else {
-		  console.log('Something went wrong when uploading the files:\n' + request.responseText);
-		}
-	  }
-	};
-	request.send(dropZone.getFormData());
-  });
-
-  $('#document-form').submit(function(event) {
-	console.log('sdfgy');
-	event.preventDefault();
-  });
-
-  dropZone.clear();
-});
-
-function uploadDocument() {
-  $.ajax({
-	type: 'POST',
-	url: '/api/documents',
-	data: $('#document-form').serialize(),
-	dataType: 'json',
-	processData: false,
-	success: function(data, textStatus, jqXHR){
-	  if (typeof data.error !== 'undefined') {
-		console.log('ERRORS: ' + data.error);
-	  }
-
-	  $('#upload-document-output').html(JSON.stringify(data));
-	  $('#doc-fields-clear').click();
-	  dropZone.clear();
-	},
-	error: function (jqXHR, textStatus, errorThrown) {
-	  console.log('ERRORS: ' + textStatus);
-	}
-  });
-}
-
 var dropZone = {
   files: [],
 
@@ -130,3 +71,74 @@ var dropZone = {
 	$('#dropZone').html(message);
   }
 };
+
+dropZone.clear();
+
+domEl('#upload').onclick = function () {
+  var request = new XMLHttpRequest();
+
+  request.open('POST', '/api/files');
+
+  request.onreadystatechange = function() {
+	if (request.readyState === XMLHttpRequest.DONE) {
+	  if (request.status === 201) {
+		let responseObj = JSON.parse(request.responseText)
+		  , responseObjKeys = Object.keys(responseObj);
+
+		for (let i=0; i<responseObjKeys.length; i++) {
+		  domEl('#file_' + i + '_path').value = responseObj[i][0].path;
+		}
+
+		uploadDocument();
+	  } else {
+		console.log('Something went wrong when uploading the files:\n' + request.responseText);
+	  }
+	}
+  };
+
+  request.send(dropZone.getFormData());
+};
+
+domEl('#doc-fields-clear').onclick = function() {
+  domEl('.doc-input-field').value = '';
+  domEl('#form-files').innerHTML = '';
+  dropZone.clear();
+};
+
+function uploadDocument() {
+  $.ajax({
+	type: 'POST',
+	url: '/api/documents',
+	data: $('#document-form').serialize(),
+	dataType: 'json',
+	processData: false,
+	success: function(data, textStatus, jqXHR){
+	  if (typeof data.error !== 'undefined') {
+		console.log('ERRORS: ' + data.error);
+	  }
+
+	  $('#upload-document-output').html(JSON.stringify(data));
+	  $('#doc-fields-clear').click();
+	  dropZone.clear();
+	},
+	error: function (jqXHR, textStatus, errorThrown) {
+	  console.log('ERRORS: ' + textStatus);
+	}
+  });
+}
+
+function domEl(identifier) {
+  if (typeof identifier !== 'string') {
+	return null;
+  }
+
+  if (identifier[0] === '#') {
+	return document.getElementById(identifier.substring(1));
+  }
+
+  if (identifier[0] === '.') {
+	return document.getElementsByClassName(identifier.substring(1));
+  }
+
+  return null;
+}
